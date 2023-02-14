ad7504109229f0d00b840f67d7d0c1940b3d7c44
Tiago Emerick <tiagoeborsanyi@gmail.com>
2023-02-14 14:54:00 -0300

    created card on homePage

--- lib/data_json/card_json.dart
+++ lib/data_json/card_json.dart
@@ -1,19 +1,24 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const List cardList = [
   {
     'title': 'Logo Dessign',
     'subtitle': 'Planned: 11 tasks',
-    'card_color': Color.fromARGB(255, 75, 226, 213)
+    'card_color': Color.fromARGB(255, 75, 226, 213),
+    'icon': Icons.logo_dev,
   },
   {
     'title': 'Website Landing Page',
     'subtitle': 'Planned: 6 tasks',
-    'card_color': Color.fromARGB(255, 207, 80, 224)
+    'card_color': Color.fromARGB(255, 207, 80, 224),
+    'icon': Icons.sports_baseball_outlined,
   },
   {
     'title': 'Mobile App UI',
     'subtitle': 'Planned: 11 tasks',
-    'card_color': Color.fromARGB(255, 206, 155, 53)
+    'card_color': Color.fromARGB(255, 206, 155, 53),
+    'icon': Icons.mobile_friendly_outlined,
   }
 ];
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_app_1/data_json/card_json.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -217,6 +218,81 @@ class _HomeState extends State<Home> {
             ],
           ),
           const SizedBox(height: 12),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: List.generate(cardList.length, (index) {
+                  return Container(
+                    height: 100,
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black87, width: 1),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 2,
+                          offset: Offset(4, 5),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            color: cardList[index]['card_color'],
+                            border: const Border(
+                              right: BorderSide(color: Colors.black, width: 1),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 1),
+                          ),
+                          child: Center(
+                            child: Icon(
+                              cardList[index]['icon'],
+                              size: 28,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              cardList[index]['title'],
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(height: 6),
+                            Text(
+                              cardList[index]['subtitle'],
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
         ],
       ),
     );
